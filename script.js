dc3fe3c88636b6fb6bf5dab1b211eeaf360e405f
John Joshua Ramirez <johnjoshuaramirez.social@gmail.com>
2022-10-10 05:10:19 -0800

    Fixed deleting li elements

--- script.js
+++ script.js
@@ -36,7 +36,18 @@ function showTasks() {
    let li = "";
 
    listArr.forEach((element, index) => {
-      li = `<li>${element} <span><i class="fas fa-trash"></i></span></li>`
+      li += `<li>${element} <span onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`
    });
-   todoList.innerHTML += li;
+   todoList.innerHTML = li;
+   inputBox.value = "";
 }
+
+function deleteTask(index) {
+   let getLocalStorage = localStorage.getItem("New Todo");
+   listArr = JSON.parse(getLocalStorage);
+   listArr.splice(index, 1);
+   localStorage.setItem("New Todo", JSON.stringify(listArr));
+
+   showTasks();
+}
+
