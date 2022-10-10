8c92ddf6b46b9e0949c2912a4338d19497cb3845
John Joshua Ramirez <johnjoshuaramirez.social@gmail.com>
2022-10-10 03:47:13 -0800

    Done setting up my local storage

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ const inputBox = document.querySelector(".inputField input"),
 addBtn = document.querySelector(".inputField button");
 
 inputBox.addEventListener("keyup", (e) => {
+   let userData = inputBox.value;
    if (e.target.value.trim().length) {
       addBtn.classList.add("active");
    } else {
       addBtn.classList.remove("active");
    }
+});
+
+addBtn.addEventListener("click", () => {
+   let userData = inputBox.value;
+   let getLocalStorage = localStorage.getItem("New Todo");
+   if (getLocalStorage === null) {
+      listArr = [];
+   } else {
+      listArr = JSON.parse(getLocalStorage);
+   }
+   listArr.push(userData);
+   localStorage.setItem("New Todo", JSON.stringify(listArr));
 });
