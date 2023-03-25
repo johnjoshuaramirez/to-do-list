874b8b8d3c21e50b4717dcb29e92907c0fed61c2
John Joshua Ramirez <johnjoshuaramirez.official@gmail.com>
2023-03-25 04:45:15 -0700

    feat: add delete logic for x-mark

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const project_items = [
    }
 ];
 
-function render_project() {
+function render_projects() {
    project_items.forEach((item, index) => {
       const li = document.createElement('li');
       li.dataset.index = index;
@@ -49,6 +49,10 @@ function render_project() {
 
       nav_list_project.appendChild(li);
    });
+}
+
+function render_tasks() {
+
 }
 
 function clear(element) {
@@ -74,31 +78,33 @@ nav_button_add.addEventListener('click', () => {
       nav_item_input_group.classList.add('hidden');
       nav_item_add_project.classList.remove('hidden');
       clear(nav_list_project);
-      render_project();
+      render_projects();
    }
 });
 
 nav_list_project.addEventListener('click', e => {
+   // Nav X-Mark Logic
    if (e.target.classList.contains('fa-xmark')) {
       const index = e.target.parentElement.dataset.index;
       project_items.splice(index, 1);
       clear(nav_list_project);
-      render_project();
+      render_projects();
       console.log(project_items);
    }
 
+   // Nav Button Logic
    if (e.target.classList.contains('nav__button--project')) {
-      // render main section with the project name and associate the add task button to the current object
       main_section_project.classList.remove('hidden');
       main_section_task.classList.add('hidden');
       clear(main_project_task_list);
 
       const target_index = e.target.parentElement.dataset.index;
       main_project_title.innerText = project_items[target_index].project;
-      main_button_add.dataset.index = target_index;
+      main_project_title.dataset.index = target_index;
 
-      project_items[target_index].tasks.forEach(task => {
+      project_items[target_index].tasks.forEach((task, index) => {
          const li = document.createElement('li');
+         li.dataset.index = index;
          li.classList.add('main__item');
          li.innerHTML = `
          <div class="main__item-left">
@@ -117,23 +123,24 @@ nav_list_project.addEventListener('click', e => {
    console.log(e.target);
 });
 
+// Main Add Task Button Logic
 main_item_add_task.addEventListener('click', () => {
    main_item_input_group.classList.remove('hidden');
    main_item_add_task.classList.add('hidden');
 });
 
+// Main Cancel Button Logic
 main_button_cancel.addEventListener('click', () => {
    main_item_input_group.classList.add('hidden');
    main_item_add_task.classList.remove('hidden');
 });
 
+// Main Add Button Logic
 main_button_add.addEventListener('click', () => {
    if (main_input.value.trim()) {
       clear(main_project_task_list);
 
-      const target_index = main_button_add.dataset.index;
-      console.log(target_index);
-
+      const target_index = main_project_title.dataset.index;
       project_items[target_index].tasks.push(main_input.value.trim());
       main_input.value = '';
 
@@ -156,4 +163,41 @@ main_button_add.addEventListener('click', () => {
    }
 });
 
-render_project();
+main_project_task_list.addEventListener('click', e => {
+   if (e.target.classList.contains('fa-square') || e.target.classList.contains('fa-square-check')) {
+      e.target.classList.toggle('fa-square');
+      e.target.classList.toggle('fa-square-check');
+      e.target.nextElementSibling.classList.toggle('cross-out');
+   }
+
+   if (e.target.classList.contains('fa-xmark')) {
+      const target_index = main_project_title.dataset.index;
+      const task_index = e.target.parentElement.dataset.index;
+      project_items[target_index].tasks.splice(task_index, 1);
+      clear(main_project_task_list);
+      // console.log(project_items[target_index].tasks)
+      // render_tasks();
+      project_items[target_index].tasks.forEach((task, index) => {
+         const li = document.createElement('li');
+         li.dataset.index = index;
+         li.classList.add('main__item');
+         li.innerHTML = `
+         <div class="main__item-left">
+				<i class="fa-regular fa-square"></i>
+				<p class="main__text">${task}</p>
+			</div>
+				<div class="main__item-right">
+				<p class="main__text main__text--due-date">No Due Date</p>
+			</div>
+			<i class="fa-solid fa-xmark"></i>
+      `;
+         main_project_task_list.appendChild(li);
+      });
+   }
+});
+
+// find a way to delete an item in the project tasks if we click x then we remove that item its array and rerender all elements
+// find a way to reference the dom tasks index into the array index
+// find a way to rerender the specic task array
+
+render_projects();
