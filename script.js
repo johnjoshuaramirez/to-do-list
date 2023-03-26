2cacb35833885f2b4fa73ea96321236820a8293b
John Joshua Ramirez <johnjoshuaramirez.official@gmail.com>
2023-03-25 19:32:12 -0700

    refactor: clean code

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-const nav_button_all_tasks = document.querySelector('[data-nav-button-all-tasks]');
-const nav_list_project = document.querySelector('[data-nav-list-project]');
-const nav_x_mark = document.querySelector('[data-nav-x-mark]');
-const nav_item_input_group = document.querySelector('[data-nav-item-input-group]');
-const nav_input = document.querySelector('[data-nav-input]');
-const nav_button_add = document.querySelector('[data-nav-button-add]');
-const nav_button_cancel = document.querySelector('[data-nav-button-cancel]');
-const nav_item_add_project = document.querySelector('[data-nav-item-add-project]');
+const add_project_button = document.querySelector('[data-nav-item-add-project');
+const project_input_group = document.querySelector('[data-nav-item-input-group]');
+const add_project_cancel_button = document.querySelector('[data-nav-button-cancel]');
+const add_project_add_button = document.querySelector('[data-nav-button-add]');
+const project_input = document.querySelector('[data-nav-input]');
+const project_list = document.querySelector('[data-nav-list-project]');
+const project_title = document.querySelector('[data-project-title]');
+const task_list = document.querySelector('[data-task-list]');
 
-const main_section_all_tasks = document.querySelector('[data-main-section-all-task]');
-const main_all_task_list = document.querySelector('[data-main-section-all-task-list]')
-const main_section_project = document.querySelector('[data-main-section-project]');
-const main_project_title = document.querySelector('[data-project-title]');
-const main_project_task_list = document.querySelector('[data-project-task-list]');
-const main_item_add_task = document.querySelector('[data-main-item-add-task]');
-const main_item_input_group = document.querySelector('[data-main-item-input-group]');
-const main_input = document.querySelector('[data-main-input]');
-const main_button_cancel = document.querySelector('[data-main-button-cancel]');
-const main_button_add = document.querySelector('[data-main-button-add]');
-
-const project_items = [
+const projects = [
    {
       project: 'Project One',
       tasks: ['Task One', 'Task Two']
@@ -26,60 +15,30 @@ const project_items = [
    {
       project: 'Project Two',
       tasks: ['Task Three', 'Task Four']
-   },
-   {
-      project: 'Project Three',
-      tasks: ['Task Five', 'Task Six']
    }
 ];
 
-const all_tasks = [];
-project_items.forEach(item => {
-   item.tasks.forEach(task => {
-      all_tasks.push(task)
-   });
-});
+const all_tasks = [
+   'Task One',
+   'Task Two',
+   'Task Three',
+   'Task Four'
+];
 
-console.log(all_tasks);
-nav_button_all_tasks.addEventListener('click', () => {
-   clear(main_all_task_list);
-   main_section_all_tasks.classList.remove('hidden');
-   main_section_project.classList.add('hidden');
-   main_project_title.innerText = 'All Tasks';
-   all_tasks.forEach((task, index) => {
-      const li = document.createElement('li');
-      li.dataset.index = index;
-      li.classList.add('main__item');
-      li.innerHTML = `
-         <div class="main__item-left">
-            <i class="fa-regular fa-square"></i>
-            <p class="main__text">${task}</p>
-         </div>
-            <div class="main__item-right">
-            <p class="main__text main__text--due-date">No Due Date</p>
-         </div>
-         <i class="fa-solid fa-xmark"></i>
-      `;
+add_project_button.addEventListener('click', show_project_input_group);
+add_project_cancel_button.addEventListener('click', hide_project_input_group);
+add_project_add_button.addEventListener('click', add_project);
+project_list.addEventListener('click', remove_project);
+project_list.addEventListener('click', show_project_tasks);
 
-      main_all_task_list.appendChild(li);
-      console.log(index);
-   });
-});
+function show_project_input_group() {
+   project_input_group.classList.remove('hidden');
+   add_project_button.classList.add('hidden');
+}
 
-function render_projects() {
-   project_items.forEach((item, index) => {
-      const li = document.createElement('li');
-      li.dataset.index = index;
-      li.classList.add('nav__item', 'nav__item--project');
-      li.innerHTML = `
-         <button class="nav__button nav__button--project">
-            <i class="fa-solid fa-list-check"></i>${item.project}
-         </button>
-         <i class="fa-solid fa-xmark"></i>
-      `;
-
-      nav_list_project.appendChild(li);
-   });
+function hide_project_input_group() {
+   project_input_group.classList.add('hidden');
+   add_project_button.classList.remove('hidden');
 }
 
 function clear(element) {
@@ -88,138 +47,76 @@ function clear(element) {
    }
 }
 
-nav_item_add_project.addEventListener('click', () => {
-   nav_item_add_project.classList.add('hidden');
-   nav_item_input_group.classList.remove('hidden');
-});
+function add_project() {
+   projects.push({
+      project: project_input.value.trim(),
+      tasks: []
+   });
 
-nav_button_cancel.addEventListener('click', () => {
-   nav_item_input_group.classList.add('hidden');
-   nav_item_add_project.classList.remove('hidden');
-});
+   project_input.value = '';
+   clear(project_list);
+   render_projects();
+}
 
-nav_button_add.addEventListener('click', () => {
-   if (nav_input.value.trim()) {
-      project_items.push({ project: nav_input.value.trim(), tasks: [] });
-      nav_input.value = '';
-      nav_item_input_group.classList.add('hidden');
-      nav_item_add_project.classList.remove('hidden');
-      clear(nav_list_project);
-      render_projects();
-   }
-});
+function render_projects() {
+   projects.forEach((item, index) => {
+      project_list.innerHTML += `
+      <li class="nav__item nav__item--project" data-index='${index}'>
+			<button class="nav__button nav__button--project">
+			   <i class="fa-solid fa-list-check"></i>${item.project}
+			</button>
+			<i class="fa-solid fa-xmark" data-nav-x-mark></i>
+		</li>`;
+   });
+}
 
-nav_list_project.addEventListener('click', e => {
-   // Nav X-Mark Logic
+function remove_project(e) {
    if (e.target.classList.contains('fa-xmark')) {
       const project_index = e.target.parentElement.dataset.index;
-      project_items.splice(project_index, 1);
-      clear(nav_list_project);
+      projects.splice(project_index, 1);
+      clear(project_list);
       render_projects();
    }
+}
 
-   // Nav Button Logic
+function show_project_tasks(e) {
    if (e.target.classList.contains('nav__button--project')) {
-      main_section_project.classList.remove('hidden');
-      clear(main_project_task_list);
-
       const project_index = e.target.parentElement.dataset.index;
-      main_project_title.innerText = project_items[project_index].project;
-      main_project_title.dataset.index = project_index;
+      project_title.innerText = projects[project_index].project;
 
-      project_items[project_index].tasks.forEach((task, index) => {
-         const li = document.createElement('li');
-         li.dataset.index = index;
-         li.classList.add('main__item');
-         li.innerHTML = `
-         <div class="main__item-left">
+      clear(task_list);
+
+      projects[project_index].tasks.forEach(task => {
+         task_list.innerHTML += `
+      <li class="main__item">
+			<div class="main__item-left">
 				<i class="fa-regular fa-square"></i>
 				<p class="main__text">${task}</p>
 			</div>
-				<div class="main__item-right">
+			<div class="main__item-right">
 				<p class="main__text main__text--due-date">No Due Date</p>
 			</div>
 			<i class="fa-solid fa-xmark"></i>
+		</li>
       `;
-
-         main_project_task_list.appendChild(li);
-      });
-   };
-});
-
-// Main Add Task Button Logic
-main_item_add_task.addEventListener('click', () => {
-   main_item_input_group.classList.remove('hidden');
-   main_item_add_task.classList.add('hidden');
-});
-
-// Main Cancel Button Logic
-main_button_cancel.addEventListener('click', () => {
-   main_item_input_group.classList.add('hidden');
-   main_item_add_task.classList.remove('hidden');
-});
-
-// Main Add Button Logic
-main_button_add.addEventListener('click', () => {
-   if (main_input.value.trim()) {
-      clear(main_project_task_list);
-
-      const project_index = main_project_title.dataset.index;
-      project_items[project_index].tasks.push(main_input.value.trim());
-      main_input.value = '';
-
-      project_items[project_index].tasks.forEach(task => {
-         const li = document.createElement('li');
-         li.classList.add('main__item');
-         li.innerHTML = `
-         <div class="main__item-left">
-            <i class="fa-regular fa-square"></i>
-            <p class="main__text">${task}</p>
-         </div>
-            <div class="main__item-right">
-            <p class="main__text main__text--due-date">No Due Date</p>
-         </div>
-         <i class="fa-solid fa-xmark"></i>
-      `;
-
-         main_project_task_list.appendChild(li);
-      });
-
-      main_item_input_group.classList.add('hidden');
-      main_item_add_task.classList.remove('hidden');
-   }
-});
-
-main_project_task_list.addEventListener('click', e => {
-   if (e.target.classList.contains('fa-square') || e.target.classList.contains('fa-square-check')) {
-      e.target.classList.toggle('fa-square');
-      e.target.classList.toggle('fa-square-check');
-      e.target.nextElementSibling.classList.toggle('cross-out');
-   }
-
-   if (e.target.classList.contains('fa-xmark')) {
-      const project_index = main_project_title.dataset.index;
-      const task_index = e.target.parentElement.dataset.index;
-      project_items[project_index].tasks.splice(task_index, 1);
-      clear(main_project_task_list);
-      project_items[project_index].tasks.forEach((task, index) => {
-         const li = document.createElement('li');
-         li.dataset.index = index;
-         li.classList.add('main__item');
-         li.innerHTML = `
-         <div class="main__item-left">
-				<i class="fa-regular fa-square"></i>
-				<p class="main__text">${task}</p>
-			</div>
-				<div class="main__item-right">
-				<p class="main__text main__text--due-date">No Due Date</p>
-			</div>
-			<i class="fa-solid fa-xmark"></i>
-      `;
-         main_project_task_list.appendChild(li);
       });
    }
-});
-
+}
 
 render_projects();
+
+all_tasks_button.addEventListener('click', show_all_tasks);
+today_button.addEventListener('click', show_today_tasks);
+this_week_button.addEventListener('click', show_this_week_tasks);
+project_list.addEventListener('click', show_project_tasks);
+add_task_cancel_button.addEventListener('click', hide_task_input_group);
+
+checkbox.addEventListener('click', toggle_check);
+add_task_button.addEventListener('click', show_task_input_group);
+add_task_add_button.addEventListener('click', add_task);
+task_xmark.addEventListener('click', remove_task);
+due_date.addEventListener('click', set_due_date);
+
+
+
+
